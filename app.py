27f3878fe3f795e5dfaeda4ccb25ddcbb3dc0d13
Ostboll <72736160+Ostboll@users.noreply.github.com>
2022-03-29 16:03:28 +0200

    Fixed writeincsv

--- app.py
+++ app.py
@@ -3,21 +3,25 @@ import matplotlib.pyplot as plt
 import pandas
 import csv
 
- 
-def writecsvrow( file, data ):
-    f = open( 'Datafiles/' + file, 'w' ) #Open file in write mode
+app = Flask(__name__)
+
+def writeincsv( file, data ):
+    '''Takes data that is a list of lists and writes them in the csv file file'''
+    f = open( 'GitHub/EmotionalFlask/DataFiles/' + file, 'w' ) #Open file in write mode
     writer = csv.writer( f ) # Create the csv file 
 
     if len( data ) == 1:
         writer.writerow( data ) # Write one row to csv
     elif len( data ) > 1: 
-        writer.writerows( data) # Write multiple rows to csv
+        writer.writerows( data ) # Write multiple rows to csv
+
     f.close()
     return f
 
 
 
-app = Flask(__name__)
+
+
 
 
 @app.route("/")
@@ -25,4 +29,9 @@ def home():
     return render_template('index.html')
 
 
+
+if __name__ =="__main__":
+    app.run
+    writeincsv( 'test.csv', [['hej'], [1, 2, 3] , [['hej'], 'hej då'] ] )
+    # writeincsv( 'test.csv', [1,2,3] )
 
