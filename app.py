7f3ecdc491f62dbb87b1d58e362c4a1c5ec27c95
Ostboll <72736160+Ostboll@users.noreply.github.com>
2022-03-29 15:41:20 +0200

    FUnction to write in a csv file

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, flash ,redirect, url_for,request
 import matplotlib.pyplot as plt
+import pandas
+import csv
 
+ 
+def writecsvrow( file, data ):
+    f = open( 'Datafiles/' + file, 'w' ) #Open file in write mode
+    writer = csv.writer( f ) # Create the csv file 
 
+    if len( data ) == 1:
+        writer.writerow( data ) # Write one row to csv
+    elif len( data ) > 1: 
+        writer.writerows( data) # Write multiple rows to csv
+    f.close()
+    return f
 
 
 
