029ce57ea49d1f188278aa7419695a18144e4647
Ostboll <72736160+Ostboll@users.noreply.github.com>
2022-03-30 09:28:18 +0200

    TOg tillbaka write in csv samt uppdelning av kod

--- app.py
+++ app.py
@@ -1,9 +1,49 @@
-from flask import Flask, render_template, flash ,redirect, url_for,request
+############################################################################################################################################
+
+import os, csv
+from flask import Flask, render_template, flash ,redirect, url_for,request,send_from_directory, send_file
 import matplotlib.pyplot as plt
-import pandas
-import csv
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = r'uploads'
+
+############################################################################################################################################
+#app funktioner
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/simulation")
+def simulation():
+    return render_template('simulation.html')
+
+@app.route('/upload', methods=['GET', 'POST'])  
+def upload():  
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('upload.html')
+
+@app.route('/download')
+def display_download():
+    items = os.listdir(r'uploads')
+    output = (items if items else 'Nothing to download :(' )
+    choice= request.args.get('datafile','')
+    if choice:
+        send_file(choice,as_attachment=True)
+    return render_template('download.html',output = output)
+
+@app.route('/download/get/<thing>')
+def something_file(thing):
+    path="uploads/"+thing
+    return send_file(path, as_attachment= True)
+
+
+############################################################################################################################################
+# Övriga funktioner 
 
 def writeincsv( file, data ):
     '''Takes data that is a list of lists and writes them in the csv file file'''
@@ -13,25 +53,20 @@ def writeincsv( file, data ):
     if len( data ) == 1:
         writer.writerow( data ) # Write one row to csv
     elif len( data ) > 1: 
+        writer.writerows( data) # Write multiple rows to csv
         writer.writerows( data ) # Write multiple rows to csv
 
     f.close()
     return f
 
 
+############################################################################################################################################
+# Run code
 
+if __name__ == '__main__':  
+    app.run(debug = True)  #Om man ändrar och sparar när servern är igång startar den om automatiskt
 
+############################################################################################################################################
 
-
-
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-
-
-if __name__ =="__main__":
-    app.run
-    writeincsv( 'test.csv', [['hej'], [1, 2, 3] , [['hej'], 'hej då'] ] )
-    # writeincsv( 'test.csv', [1,2,3] )
-
+# $env:FLASK_APP = "hello"
+# $ flask run
