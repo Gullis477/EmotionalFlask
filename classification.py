7429ad7b57a085309be990153d631ccfeaba55ec
Sofia Gimholt <sofia@gimholt.se>
2022-03-30 13:50:04 +0200

    Fix

--- classification.py
+++ classification.py
@@ -3,6 +3,10 @@ import pickle
 import random
 import matplotlib.pyplot as plt
 import time
+import handelCSV
+
+# ------------------------------------------------------------------------------
+
 
 def fake_data():
     '''
@@ -11,31 +15,32 @@ def fake_data():
     data = [random.random() for _ in range(4)]
     return data
 
-def classify(data,model):
+
+def classify(data, model):
     '''Classify the the input data with the input model and returns a string with the classification'''
     predicted_class = model.predict([data])
     return predicted_class[0]
 
-def bar_chart_arousal(data,names,classification):
+
+def bar_chart_arousal(data, names, classification):
     '''Creates a barchart with the input data and the names as column names, the input classification will go in the title '''
     
     plt.clf()
-    plt.bar(names,data)
+    plt.bar(names, data)
     plt.title('Your Arousal is: '+classification)
     plt.xlabel('Brainwave')
     plt.ylabel('BrainDamage')
     plt.savefig('static/eyy.png')
 
 
-
+# ------------------------------------------------------------------------------
 # load the model from disk
+
+
 loaded_model = pickle.load(open('test_algoritm.sav', 'rb'))
-names = ['alpha_bin','beta_bin','gamma_bin','delta_bin']
+names = ['alpha_bin', 'beta_bin', 'gamma_bin', 'delta_bin']
 for _ in range(10):
     dataset = fake_data()
-    result = classify(dataset,loaded_model)
-    bar_chart_arousal(dataset,names,result)
+    result = classify(dataset, loaded_model)
+    bar_chart_arousal(dataset, names, result)
     time.sleep(3)
-
-
-
--- handelCSV.py
+++ handelCSV.py
@@ -1,13 +1,28 @@
 from csv import writer
+import pandas as pd
 
 
-def writeincsv( file, data ):
+def write_in_csv(file, data):
     '''Takes data that is a list of lists and writes them in the csv file file'''
-    if len( data ) == 1:
-        writer.writerow( data ) # Write one row to csv
-    elif len( data ) > 1: 
-        writer.writerows( data) # Write multiple rows to csv
-        writer.writerows( data ) # Write multiple rows to csv
+    if len(data) == 1:
+        writer.writerow(data)  # Write one row to csv
+    elif len(data) > 1:
+        writer.writerows(data)  # Write multiple rows to csv
+        writer.writerows(data)  # Write multiple rows to csv
 
     file.close()
     return file
+
+
+def load_csv_file(file_name):
+    data_frame = None
+    try:
+        data_frame = pd.DataFrame()
+        data = pd.DataFrame(pd.read_csv(file_name, dtype='category', sep=","))
+
+        data_frame = data_frame.append(data)
+
+    except FileNotFoundError as Not_Found_Error:
+        data_frame = pd.DataFrame()
+        print(f"Error Message: {Not_Found_Error}")
+    return data_frame
