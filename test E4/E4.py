5f3d64a8b30f89f55aba43041ea30bd32856e599
Ostboll <72736160+Ostboll@users.noreply.github.com>
2022-04-14 18:42:51 +0200

    graph bvp

---
+++ test E4/E4.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from pandas import read_csv
+
+import numpy as np
+
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import *
+
+import matplotlib.animation as animation 
+
+import ipywidgets as widgets
+from ipywidgets import interact 
+
+
+
+# Import CSV as dfs:
+mapp=  "csvfiles/E4.try/"
+bvp=   pd.DataFrame( read_csv( mapp + "BVP.csv" ) )     #
+eda=   pd.DataFrame( read_csv( mapp + "EDA.csv" ) )     #
+hr=    pd.DataFrame( read_csv( mapp + "HR.csv" ) )      # r2 = sample rate in Hz
+ibi=   pd.DataFrame( read_csv( mapp + "IBI.csv" ) )     # col1 = time, col2 = durations in sec
+temp=  pd.DataFrame( read_csv( mapp + "TEMP.csv" ) )    # 
+
+
+
+# Graphs 
+def graph_bvp( bvp=bvp ):
+    # header = str( 16443467447.0000 )
+    x = [ i for i in range( len( bvp[2:] ) ) ]
+    y = bvp[ 2: ]
+
+    data_linreg = np.polyfit( x, y, 3 )
+    xp = np.linspace( 5, 33000, 3000)
+    pr = np.polyval( data_linreg, xp)
+
+    fig = plt.figure( figsize=( 9, 5 ) )
+    ax = fig.add_subplot()
+    ax.grid()
+    ax.set_xlabel( "Timestamp" )
+    ax.set_ylabel( "BVP" )
+    ax.set_title( " BVP Measure" )
+
+    ax.scatter( x, y, color='lightgray', s=1 )
+    ax.plot( xp, pr )
+    plt.show()
+
+
+graph_bvp()
+
+
